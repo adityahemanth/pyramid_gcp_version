b5402920e97e9815775e3f13c217dee4a46bc58b
Hemanth Aditya <chad@Hemanths-MacBook-Pro.local>
2016-09-21 23:13:00 -0700

    navigation changes

--- bootstrap/js/nodes.js
+++ bootstrap/js/nodes.js
@@ -1,4 +1,7 @@
 function set_data(data) {
+
+
+	list.empty()
 	$.get('/lcco', {
 	 	request : data
 	}, function(data, status){
@@ -8,7 +11,7 @@ function set_data(data) {
 			list.empty()
 			for(i = 0; i < data.children.length; i++){
 				list.append(
-					$('<li>').attr('class', 'list-group-item').append(
+					$('<li>').attr('class', 'list-group-item btn btn-default text-left').append(
 						$('<p>').attr('class', 'node-item').append(data.children[i].LCCN , ' : ', data.children[i].description)
 					)
 				)
@@ -23,7 +26,7 @@ function set_back(){
 $('#back_list').empty()
 	for(i = 0; i < back.length; i++){
 	  $('#back_list').append(
-	    $('<li>').attr('class', 'back-item').append(back[i])
+	    $('<li>').attr('class', 'back-item btn btn-primary').attr('style', 'margin:3px').append(back[i])
 	    )
 	}
 }
